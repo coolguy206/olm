d24de666da28d8ae6cff2555e494a697bfde3ee3
coolguy206 <davidthanphilom@msn.com>
2022-10-21 14:21:24 -0700

    added sign up genius api test and re-coded js to use fetch main

--- js/browserify/scripts.js
+++ js/browserify/scripts.js
@@ -11,21 +11,121 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 // dotenv.config();
 // console.log(process.env);
 // console.log(process.env)
-var GOOGLE_API_KEY = "AIzaSyD-bqqEqVuD1nhHqjozx5VnDAwKMGgpE7c"; //? to test error
+var GOOGLE_API_KEY = "AIzaSyD-bqqEqVuD1nhHqjozx5VnDAwKMGgpE7c";
+var SIGNUP_GENIUS_KEY = "?user_key=V0FzMkxZcmVOZlVnclZMVEl6dGhWQT09"; //? to test error
 // var GOOGLE_API_KEY = "xyz";
+// var SIGNUP_GENIUS_KEY = "xyz";
 
 var sheet_id = "1-2g3tZj_hjCnSxLquhs3uAAXTm4UZQ8AxDBPCvzKcTE";
 var baseURL = "https://sheets.googleapis.com/v4/spreadsheets/";
 var range = "A:Z";
+var sGBaseURL = "https://api.signupgenius.com/v2/k/";
+var sGSignUps = "signups/created/all/";
+var sGReports = "signups/report/all/";
 var message = "";
 (0, _jquery["default"])(document).ready(function () {
   console.log("page ready");
+  var signUpArr = {
+    genius: [],
+    google: []
+  }; //? GET SIGNUPS FROM SIGNUP GENIUS
 
-  _jquery["default"].get("".concat(baseURL).concat(sheet_id, "?key=").concat(GOOGLE_API_KEY, "&includeGridData=true"), function (data) {
+  fetch("".concat(sGBaseURL).concat(sGSignUps).concat(SIGNUP_GENIUS_KEY)).then(function (data) {
+    return data.json();
+  }).then(function (data) {
+    // console.log(data);
+    data.data.map(function (val, i) {
+      // console.log(val);
+      var d = val.startdatestring;
+      d = d.split(" ")[0]; // console.log(d);
+
+      var date = new Date(d).toDateString();
+      var obj = {
+        title: val.title,
+        id: val.signupid,
+        url: val.signupurl,
+        img: val.mainimage,
+        date: date
+      };
+      signUpArr.genius.push(obj); // console.log(obj);
+    }); // console.log(`signUpArr`);
+    // console.log(signUpArr);
+
+    return signUpArr; //? GET THE REPORTS FROM SIGNUP GENIUS
+  }).then(function (signUpArr) {
+    signUpArr.genius.map(function (val, i) {
+      fetch("".concat(sGBaseURL).concat(sGReports).concat(val.id, "/").concat(SIGNUP_GENIUS_KEY)).then(function (data) {
+        return data.json();
+      }).then(function (data) {
+        // console.log(`reports`);
+        // console.log(data);
+        // console.log(`signup`);
+        val.signUps = []; // console.log(val);
+
+        data.data.signup.map(function (arr, j) {
+          if (arr.email !== "") {
+            var obj = {
+              amountpaid: arr.amountpaid,
+              email: arr.email,
+              firstname: arr.firstname,
+              lastname: arr.lastname,
+              phone: arr.phone,
+              item: arr.item,
+              comments: arr.comment
+            };
+            val.signUps.push(obj);
+          }
+        }); // console.log(val);
+      })["catch"](function (err) {
+        console.log("oops something went wrong");
+        console.log(err);
+        (0, _jquery["default"])('.the-error').show();
+        (0, _jquery["default"])(".the-form").hide();
+      }); // console.log(`outside fetch`);
+      // console.log(signUpArr);
+    });
+    return signUpArr; //? GET GOOGLE SHEETS
+  }).then(function (signUpArr) {
+    fetch("".concat(baseURL).concat(sheet_id, "?key=").concat(GOOGLE_API_KEY, "&includeGridData=true")).then(function (data) {
+      return data.json();
+    }).then(function (data) {
+      console.log("google sheets");
+      console.log(data);
+      data.sheets.map(function (val, i) {
+        var obj = {
+          title: val.properties.title,
+          signUps: []
+        };
+        val.data[0].rowData.map(function (arr, j) {
+          if (j !== 0) {
+            if (arr.values[0].formattedValue !== undefined) {
+              var d = new Date(arr.values[0].formattedValue).toDateString();
+              var signUpObj = {
+                date: d,
+                email: arr.values[1].formattedValue,
+                name: arr.values[2].formattedValue,
+                kidname: arr.values[3].formattedValue,
+                grade: arr.values[4].formattedValue
+              };
+              obj.signUps.push(signUpObj);
+            }
+          }
+        });
+        signUpArr.google.push(obj);
+      }); // console.log(signUpArr);
+      // return signUpArr;
+    })["catch"](function (err) {
+      console.log("oops something went wrong");
+      console.log(err);
+      (0, _jquery["default"])('.the-error').show();
+      (0, _jquery["default"])(".the-form").hide();
+    });
+    ;
+    return signUpArr;
+  }).then(function (signUpArr) {
+    console.log(signUpArr);
     console.log("all good");
-    console.log(data);
-    (0, _jquery["default"])('.the-form').show();
-    var theSheets = data.sheets;
+    (0, _jquery["default"])(".the-form").show();
     (0, _jquery["default"])('form').on('submit', function (e) {
       e.preventDefault(); //! reset message var
 
@@ -35,65 +135,68 @@ var message = "";
       var theEmail1 = (0, _jquery["default"])('input[name="email1"]').val().toLowerCase();
       var theEmail2 = (0, _jquery["default"])('input[name="email2"]').val().toLowerCase(); // console.log("🚀 ~ file: scripts.js ~ line 52 ~ $ ~ theEmail", theEmail1)
       // console.log("🚀 ~ file: scripts.js ~ line 53 ~ $ ~ theEmail", theEmail2)
-      //?execute the loop
 
-      _jquery["default"].each(theSheets, function (i, val) {
-        // console.log("🚀 ~ file: scripts.js ~ line 43 ~ $.each ~ val", val)
-        var theEvent = val.properties.title; // console.log("🚀 ~ file: scripts.js ~ line 47 ~ $.each ~ theEvent", theEvent);
-        //? loop the rowData to find email
+      if (theEmail1 !== "" || theEmail2 !== "") {
+        //?execute the loop
+        signUpArr.genius.map(function (val, i) {
+          var theEvent = val.title;
+          var theEventURL = val.url;
+          var theEventDate = val.date;
+          var theEventImg = val.img;
+          val.signUps.map(function (arr, j) {
+            var thisEmail = arr.email.toLowerCase();
 
-        _jquery["default"].each(val.data[0].rowData, function (j, str) {
-          if (j !== 0) {
-            var thisDate = str.values[0].formattedValue;
-            var thisEmail = str.values[1].formattedValue;
-            var thisName = str.values[2].formattedValue;
-            var thisChild = str.values[3].formattedValue;
-            var thisGrade = str.values[4].formattedValue; // console.log("🚀 ~ file: scripts.js ~ line 53 ~ $.each ~ thisEmail", thisEmail);
-
-            if (thisEmail !== undefined) {
-              thisEmail = thisEmail.toLowerCase();
-
-              if (thisEmail == theEmail1 || thisEmail == theEmail2) {
-                // console.log(`match`);
-                // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail1);
-                // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail2);
-                var elem = "<li>\n                                    <h3>".concat(theEvent, "</h3>\n                                    <h4>Email: ").concat(thisEmail, "</h4>\n                                    <h4>Name: ").concat(thisName, "</h4>\n                                    <h4>Child: ").concat(thisChild, "</h4>\n                                    <h4>Grade: ").concat(thisGrade, "</h4>\n                                    <h4>Date: ").concat(thisDate, "</h4>\n                                </li>");
-                message += elem;
-              }
+            if (thisEmail == theEmail1 || thisEmail == theEmail2) {
+              var elem = "<li>\n                        <a href=\"".concat(theEventURL, "\" target=\"_blank\">\n                            <img src=\"").concat(theEventImg, "\" alt=\"").concat(theEvent, "\">\n                            <h3>").concat(theEvent, "</h3>\n                        </a>\n                        <h4>Email: ").concat(arr.email, "</h4>\n                        <h4>Name: ").concat(arr.firstname, " ").concat(arr.lastname, "</h4>\n                        <h4>Amount Paid: ").concat(arr.amountpaid, "</h4>\n                        <h4>Item: ").concat(arr.item, "</h4>\n                        <h4>Comments: ").concat(arr.comments, "</h4>\n                        <h4>Phone: ").concat(arr.phone, "</h4>\n                        <!-- <h4>Child: ").concat(arr.kidname, "</h4>\n                        <h4>Grade: ").concat(arr.grade, "</h4>\n                        <h4>Date: ").concat(arr.date, "</h4> -->\n                    </li>");
+              message += elem;
             }
-          }
+          });
         });
-      }); //!clear all previous submissions
+        signUpArr.google.map(function (val, i) {
+          var theEvent = val.title;
+          val.signUps.map(function (arr, j) {
+            var thisEmail = arr.email.toLowerCase();
+
+            if (thisEmail == theEmail1 || thisEmail == theEmail2) {
+              // console.log(`match`);
+              // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail1);
+              // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail2);
+              var elem = "<li>\n                            <h3>".concat(theEvent, "</h3>\n                            <h4>Email: ").concat(arr.email, "</h4>\n                            <h4>Name: ").concat(arr.name, "</h4>\n                            <h4>Child: ").concat(arr.kidname, "</h4>\n                            <h4>Grade: ").concat(arr.grade, "</h4>\n                            <h4>Date: ").concat(arr.date, "</h4>\n                        </li>");
+              message += elem;
+            }
+          });
+        }); //!clear all previous submissions
+
+        (0, _jquery["default"])('ul').empty();
+
+        if (message !== "") {
+          //?display on page
+          (0, _jquery["default"])('ul').append(message);
+          (0, _jquery["default"])('ul').css('display', 'flex');
+        } else {
+          //!display error message
+          (0, _jquery["default"])('.error-message').show();
+        } //! find duplicates and highlight them
 
 
-      (0, _jquery["default"])('ul').empty();
+        var titles = (0, _jquery["default"])('ul h3'); // console.log("🚀 ~ file: scripts.js ~ line 99 ~ titles", titles)
 
-      if (message !== "") {
-        //?display on page
-        (0, _jquery["default"])('ul').append(message);
-        (0, _jquery["default"])('ul').css('display', 'flex');
-      } else {
-        //!display error message
-        (0, _jquery["default"])('.error-message').show();
-      } //! find duplicates and highlight them
-
-
-      var titles = (0, _jquery["default"])('ul h3'); // console.log("🚀 ~ file: scripts.js ~ line 99 ~ titles", titles)
-
-      for (var i = 0; i < titles.length - 1; i++) {
-        if (titles[i + 1].innerHTML == titles[i].innerHTML) {
-          // console.log(`match`);
-          // console.log(titles[i + 1].innerHTML);
-          // console.log(titles[i].innerHTML);
-          (0, _jquery["default"])(titles[i + 1]).closest('li').addClass('duplicate');
-          (0, _jquery["default"])(titles[i]).closest('li').addClass('duplicate');
+        for (var i = 0; i < titles.length - 1; i++) {
+          if (titles[i + 1].innerHTML == titles[i].innerHTML) {
+            // console.log(`match`);
+            // console.log(titles[i + 1].innerHTML);
+            // console.log(titles[i].innerHTML);
+            (0, _jquery["default"])(titles[i + 1]).closest('li').addClass('duplicate');
+            (0, _jquery["default"])(titles[i]).closest('li').addClass('duplicate');
+          }
         }
       }
-    }); //! if .get fails show the error message to contact developer
-  }).fail(function (err) {
-    console.log("something went wrong"); // console.log(err);
-
+    });
+  })["catch"](function (err) {
+    console.log("oops something went wrong");
+    console.log(err);
     (0, _jquery["default"])('.the-error').show();
+    (0, _jquery["default"])(".the-form").hide();
   });
 });
 
--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,136 @@ import $ from "jquery";
 // console.log(process.env)
 
 var GOOGLE_API_KEY = "AIzaSyD-bqqEqVuD1nhHqjozx5VnDAwKMGgpE7c";
+var SIGNUP_GENIUS_KEY = `?user_key=V0FzMkxZcmVOZlVnclZMVEl6dGhWQT09`;
+
 //? to test error
 // var GOOGLE_API_KEY = "xyz";
+// var SIGNUP_GENIUS_KEY = "xyz";
+
 var sheet_id = `1-2g3tZj_hjCnSxLquhs3uAAXTm4UZQ8AxDBPCvzKcTE`;
 var baseURL = `https://sheets.googleapis.com/v4/spreadsheets/`;
 var range = `A:Z`;
+
+var sGBaseURL = `https://api.signupgenius.com/v2/k/`;
+var sGSignUps = `signups/created/all/`;
+var sGReports = `signups/report/all/`;
 
 
 var message = ``;
 
 $(document).ready(function () {
     console.log(`page ready`);
-    $.get(`${baseURL}${sheet_id}?key=${GOOGLE_API_KEY}&includeGridData=true`, function (data) {
 
+    var signUpArr = {
+        genius: [],
+        google: []
+    };
+
+    //? GET SIGNUPS FROM SIGNUP GENIUS
+    fetch(`${sGBaseURL}${sGSignUps}${SIGNUP_GENIUS_KEY}`).then((data) => { return data.json() }).then((data) => {
+        // console.log(data);
+
+        data.data.map((val, i) => {
+            // console.log(val);
+            var d = val.startdatestring;
+            d = d.split(` `)[0];
+            // console.log(d);
+            var date = new Date(d).toDateString();
+            var obj = {
+                title: val.title,
+                id: val.signupid,
+                url: val.signupurl,
+                img: val.mainimage,
+                date: date
+            }
+            signUpArr.genius.push(obj);
+            // console.log(obj);
+        });
+        // console.log(`signUpArr`);
+        // console.log(signUpArr);
+
+        return signUpArr;
+
+        //? GET THE REPORTS FROM SIGNUP GENIUS
+    }).then((signUpArr) => {
+        signUpArr.genius.map((val, i) => {
+            fetch(`${sGBaseURL}${sGReports}${val.id}/${SIGNUP_GENIUS_KEY}`).then((data) => { return data.json() }).then((data) => {
+                // console.log(`reports`);
+                // console.log(data);
+                // console.log(`signup`);
+
+                val.signUps = [];
+                // console.log(val);
+                data.data.signup.map((arr, j) => {
+                    if (arr.email !== ``) {
+                        var obj = {
+                            amountpaid: arr.amountpaid,
+                            email: arr.email,
+                            firstname: arr.firstname,
+                            lastname: arr.lastname,
+                            phone: arr.phone,
+                            item: arr.item,
+                            comments: arr.comment
+                        }
+                        val.signUps.push(obj);
+                    }
+                });
+                // console.log(val);
+            }).catch((err) => {
+                console.log(`oops something went wrong`);
+                console.log(err);
+                $('.the-error').show();
+                $(`.the-form`).hide();
+            });
+            // console.log(`outside fetch`);
+            // console.log(signUpArr);
+        });
+
+        return signUpArr;
+
+        //? GET GOOGLE SHEETS
+    }).then((signUpArr) => {
+        fetch(`${baseURL}${sheet_id}?key=${GOOGLE_API_KEY}&includeGridData=true`).then((data) => { return data.json() }).then((data) => {
+            console.log(`google sheets`);
+            console.log(data);
+
+            data.sheets.map((val, i) => {
+                var obj = {
+                    title: val.properties.title,
+                    signUps: []
+                }
+                val.data[0].rowData.map((arr, j) => {
+                    if (j !== 0) {
+                        if (arr.values[0].formattedValue !== undefined) {
+                            var d = new Date(arr.values[0].formattedValue).toDateString()
+                            var signUpObj = {
+                                date: d,
+                                email: arr.values[1].formattedValue,
+                                name: arr.values[2].formattedValue,
+                                kidname: arr.values[3].formattedValue,
+                                grade: arr.values[4].formattedValue,
+                            }
+                            obj.signUps.push(signUpObj);
+                        }
+                    }
+                });
+                signUpArr.google.push(obj);
+            });
+            // console.log(signUpArr);
+            // return signUpArr;
+        }).catch((err) => {
+            console.log(`oops something went wrong`);
+            console.log(err);
+            $('.the-error').show();
+            $(`.the-form`).hide();
+        });;
+
+        return signUpArr;
+
+    }).then((signUpArr) => {
+        console.log(signUpArr);
         console.log(`all good`);
-        console.log(data);
-        $('.the-form').show();
-
-        var theSheets = data.sheets;
+        $(`.the-form`).show();
 
         $('form').on('submit', function (e) {
             e.preventDefault();
@@ -42,97 +154,103 @@ $(document).ready(function () {
             // console.log("🚀 ~ file: scripts.js ~ line 52 ~ $ ~ theEmail", theEmail1)
             // console.log("🚀 ~ file: scripts.js ~ line 53 ~ $ ~ theEmail", theEmail2)
 
-            //?execute the loop
-            $.each(theSheets, function (i, val) {
-                // console.log("🚀 ~ file: scripts.js ~ line 43 ~ $.each ~ val", val)
+            if (theEmail1 !== `` || theEmail2 !== ``) {
 
-                var theEvent = val.properties.title;
-                // console.log("🚀 ~ file: scripts.js ~ line 47 ~ $.each ~ theEvent", theEvent);
+                //?execute the loop
+                signUpArr.genius.map((val, i) => {
+                    var theEvent = val.title;
+                    var theEventURL = val.url;
+                    var theEventDate = val.date;
+                    var theEventImg = val.img;
 
-                //? loop the rowData to find email
-                $.each(val.data[0].rowData, function (j, str) {
+                    val.signUps.map((arr, j) => {
+                        var thisEmail = arr.email.toLowerCase();
+                        if (thisEmail == theEmail1 || thisEmail == theEmail2) {
+                            var elem = `<li>
+                        <a href="${theEventURL}" target="_blank">
+                            <img src="${theEventImg}" alt="${theEvent}">
+                            <h3>${theEvent}</h3>
+                        </a>
+                        <h4>Email: ${arr.email}</h4>
+                        <h4>Name: ${arr.firstname} ${arr.lastname}</h4>
+                        <h4>Amount Paid: ${arr.amountpaid}</h4>
+                        <h4>Item: ${arr.item}</h4>
+                        <h4>Comments: ${arr.comments}</h4>
+                        <h4>Phone: ${arr.phone}</h4>
+                        <!-- <h4>Child: ${arr.kidname}</h4>
+                        <h4>Grade: ${arr.grade}</h4>
+                        <h4>Date: ${arr.date}</h4> -->
+                    </li>`;
 
-                    if (j !== 0) {
-
-                        var thisDate = str.values[0].formattedValue;
-                        var thisEmail = str.values[1].formattedValue;
-                        var thisName = str.values[2].formattedValue;
-                        var thisChild = str.values[3].formattedValue;
-                        var thisGrade = str.values[4].formattedValue;
-                        // console.log("🚀 ~ file: scripts.js ~ line 53 ~ $.each ~ thisEmail", thisEmail);
-
-                        if (thisEmail !== undefined) {
-
-                            thisEmail = thisEmail.toLowerCase();
-
-                            if (thisEmail == theEmail1 || thisEmail == theEmail2) {
-                                // console.log(`match`);
-                                // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail1);
-                                // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail2);
-                                var elem = `<li>
-                                    <h3>${theEvent}</h3>
-                                    <h4>Email: ${thisEmail}</h4>
-                                    <h4>Name: ${thisName}</h4>
-                                    <h4>Child: ${thisChild}</h4>
-                                    <h4>Grade: ${thisGrade}</h4>
-                                    <h4>Date: ${thisDate}</h4>
-                                </li>`;
-
-                                message += elem;
-
-                            }
-
+                            message += elem;
                         }
-
-                    }
-
+                    });
                 });
 
-            });
+                signUpArr.google.map((val, i) => {
+                    var theEvent = val.title;
+                    val.signUps.map((arr, j) => {
+                        var thisEmail = arr.email.toLowerCase();
+                        if (thisEmail == theEmail1 || thisEmail == theEmail2) {
+                            // console.log(`match`);
+                            // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail1);
+                            // console.log("🚀 ~ file: scripts.js ~ line 56 ~ $.each ~ thisEmail == theEmail", thisEmail == theEmail2);
+                            var elem = `<li>
+                            <h3>${theEvent}</h3>
+                            <h4>Email: ${arr.email}</h4>
+                            <h4>Name: ${arr.name}</h4>
+                            <h4>Child: ${arr.kidname}</h4>
+                            <h4>Grade: ${arr.grade}</h4>
+                            <h4>Date: ${arr.date}</h4>
+                        </li>`;
 
-            //!clear all previous submissions
-            $('ul').empty();
+                            message += elem;
 
-            if (message !== ``) {
+                        }
+                    });
+                })
 
-                //?display on page
-                $('ul').append(message);
-                $('ul').css('display', 'flex');
+                //!clear all previous submissions
+                $('ul').empty();
 
-            } else {
+                if (message !== ``) {
+                    //?display on page
+                    $('ul').append(message);
+                    $('ul').css('display', 'flex');
+                } else {
+                    //!display error message
+                    $('.error-message').show();
+                }
 
-                //!display error message
-                $('.error-message').show();
+                //! find duplicates and highlight them
+                var titles = $('ul h3');
+                // console.log("🚀 ~ file: scripts.js ~ line 99 ~ titles", titles)
 
-            }
+                for (var i = 0; i < titles.length - 1; i++) {
 
-            //! find duplicates and highlight them
-            var titles = $('ul h3');
-            // console.log("🚀 ~ file: scripts.js ~ line 99 ~ titles", titles)
+                    if (titles[i + 1].innerHTML == titles[i].innerHTML) {
+                        // console.log(`match`);
+                        // console.log(titles[i + 1].innerHTML);
+                        // console.log(titles[i].innerHTML);
 
-            for (var i = 0; i < titles.length - 1; i++) {
-
-                if (titles[i + 1].innerHTML == titles[i].innerHTML) {
-                    // console.log(`match`);
-                    // console.log(titles[i + 1].innerHTML);
-                    // console.log(titles[i].innerHTML);
-
-                    $(titles[i + 1]).closest('li').addClass('duplicate');
-                    $(titles[i]).closest('li').addClass('duplicate');
+                        $(titles[i + 1]).closest('li').addClass('duplicate');
+                        $(titles[i]).closest('li').addClass('duplicate');
+                    }
                 }
             }
 
+
         });
 
-        //! if .get fails show the error message to contact developer
-    }).fail(function (err) {
-
-        console.log(`something went wrong`);
-        // console.log(err);
+    }).catch((err) => {
+        console.log(`oops something went wrong`);
+        console.log(err);
         $('.the-error').show();
-
+        $(`.the-form`).hide();
     });
 
+
 });
+
 
 
